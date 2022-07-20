11b537c701193b83e3da6a8a1decd3b585d1f403
Thomas Huisman <doedelenthomas@gmail.com>
2022-07-20 02:08:56 +0200

    backend: add cors

--- backend/server/routes/routes.go
+++ backend/server/routes/routes.go
@@ -2,10 +2,12 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/thohui/watchtogether/store"
 )
 
 func Setup(app *fiber.App, store *store.Store) {
+	app.Use(cors.New())
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
