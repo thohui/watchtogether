eefbfd9be04bb48c2dd5603a38c8131d3576bddc
Thomas Huisman <doedelenthomas@gmail.com>
2022-08-17 02:12:34 +0200

    frontend: update tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["dark"],
+  },
 };
