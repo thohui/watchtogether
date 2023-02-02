e81a9c1773da80d371f29fb0db950f6f2a4f8ff8
Thomas Huisman <contact@thohui.com>
2023-02-02 02:05:45 +0100

    refactor: use rwmutex

--- backend/store/store.go
+++ backend/store/store.go
@@ -8,14 +8,14 @@ import (
 )
 
 type Store struct {
-	mutex        sync.Mutex
+	mutex        sync.RWMutex
 	rooms        map[string]*room.Room
 	shutdownChan chan string
 }
 
 func New() *Store {
 	store := &Store{
-		mutex:        sync.Mutex{},
+		mutex:        sync.RWMutex{},
 		rooms:        make(map[string]*room.Room),
 		shutdownChan: make(chan string),
 	}
@@ -23,28 +23,29 @@ func New() *Store {
 	return store
 }
 
-func (store *Store) Add(room *room.Room) {
-	store.mutex.Lock()
-	defer store.mutex.Unlock()
-	room.ShutdownChan = store.shutdownChan
-	store.rooms[room.Id] = room
-}
-func (store *Store) Remove(id string) {
-	store.mutex.Lock()
-	defer store.mutex.Unlock()
-	delete(store.rooms, id)
+func (s *Store) Add(room *room.Room) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	room.ShutdownChan = s.shutdownChan
+	s.rooms[room.Id] = room
 }
 
-func (store *Store) Get(id string) *room.Room {
-	store.mutex.Lock()
-	defer store.mutex.Unlock()
-	return store.rooms[id]
+func (s *Store) Remove(id string) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	delete(s.rooms, id)
 }
 
-func (store *Store) startStoreTask() {
+func (s *Store) Get(id string) *room.Room {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+	return s.rooms[id]
+}
+
+func (s *Store) startStoreTask() {
 	for {
-		id := <-store.shutdownChan
+		id := <-s.shutdownChan
 		fmt.Printf("removed room %v from store\n", id)
-		store.Remove(id)
+		s.Remove(id)
 	}
 }
